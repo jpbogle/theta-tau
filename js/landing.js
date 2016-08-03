0a81494f8c8b89b2f986c610476536c05a631955
Jeremy Bogle <jbogle@mit.edu>
2016-08-03 14:58:03 -0400

    messing with bios

--- js/landing.js
+++ js/landing.js
@@ -1,3 +1,15 @@
 /**
  * Created by Jeremy on 7/25/16.
  */
+
+$( document ).ready(function() {
+    $('#Title').hover(
+        function () {
+            $('.dropdown').css('opacity', '1');
+            $('.link').css('transform', 'scale(1.03')
+        },
+        function () {
+            $('.dropdown').css('opacity', '0');
+            $('.link').css('transform', 'scale(1')
+        });
+});
